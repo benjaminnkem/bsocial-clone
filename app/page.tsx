7fa93a1eaf3c8a77b70e1411af5cb6f6c5dd7ef1
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-09-15 20:54:01 +0100

    changed extx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import Navbar from "@/components/UI/Navbar";
 
 export default function Home() {
   return (
     <main>
-      <h1>BSocial Clone Initial</h1>
+      <Navbar isTransparent={true} />
+      <header className="-my-10 min-h-[40rem]">
+        <div className="md:max-w-contain w-11/12 mx-auto relative">
+
+          <h1>BSocial Clone</h1>
+        </div>
+      </header>
     </main>
   );
 }
